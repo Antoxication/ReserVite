75f0aee51a2f4603daf364811e570b601d369ef2
Antonin Claudel <antonin.claudel0402@gmail.com>
2024-09-07 13:45:12 -0400

    Add Reservation and ReservationTest classes

---
+++ src/main/java/ca/uqac/ReserVite/Reservation.java
@@ -0,0 +1,22 @@
+package ca.uqac.ReserVite;
+
+import java.util.Date;
+
+public record Reservation(Client client, Chambre chambre, Date dateArrivee, Date dateDepart) {
+
+    // Annuler la réservation
+    public void annuler() {
+        this.chambre.setDisponible(true);  // Remet la chambre comme disponible
+    }
+
+    // Méthode toString pour une représentation textuelle de l'objet
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "client=" + client +
+                ", chambre=" + chambre +
+                ", dateArrivee=" + dateArrivee +
+                ", dateDepart=" + dateDepart +
+                '}';
+    }
+}
