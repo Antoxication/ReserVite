6a1c4ff0768002f0de088d00f27a4732212dbc73
Antonin Claudel <antonin.claudel0402@gmail.com>
2024-09-10 16:02:09 -0400

    Refactor Region toString method to handle empty string values

--- src/main/java/ca/uqac/ReserVite/Region.java
+++ src/main/java/ca/uqac/ReserVite/Region.java
@@ -5,11 +5,11 @@ public record Region(String pays, String province, String ville, String quartier
     @Override
     public String toString() {
         return "Region{" +
-                "pays='" + (pays != null ? pays : "Indifférent") + '\'' +
-                ", province='" + (province != null ? province : "Indifférent") + '\'' +
-                ", ville='" + (ville != null ? ville : "Indifférent") + '\'' +
-                ", quartier='" + (quartier != null ? quartier : "Indifférent") + '\'' +
-                ", rue='" + (rue != null ? rue : "Indifférent") + '\'' +
+                "pays='" + (pays != "" ? pays : "Indifférent") + '\'' +
+                ", province='" + (province != "" ? province : "Indifférent") + '\'' +
+                ", ville='" + (ville != "" ? ville : "Indifférent") + '\'' +
+                ", quartier='" + (quartier != "" ? quartier : "Indifférent") + '\'' +
+                ", rue='" + (rue != "" ? rue : "Indifférent") + '\'' +
                 '}';
     }
 }
